fe83e02742fce82f27153a0f1eec9b62545ddd92
jasonmueller <jmuel9999@gmail.com>
2021-03-31 15:18:14 -0500

    Enchanced fetch request function to be dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,14 @@
 
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-    //console.log("function was called");
+var getUserRepos = function(user) {
+    // format the github API url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos(user);
